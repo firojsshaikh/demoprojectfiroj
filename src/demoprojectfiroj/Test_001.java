f3a8823118df04bbaf89ae44a6a3d03b95cce7fb
hp <hp@DESKTOP-0I2J03I>
2022-08-07 14:56:57 +0530

    this is new framwork use it

---
+++ src/demoprojectfiroj/Test_001.java
@@ -0,0 +1,14 @@
+package demoprojectfiroj;
+
+public class Test_001 {
+
+	public Test_001() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
